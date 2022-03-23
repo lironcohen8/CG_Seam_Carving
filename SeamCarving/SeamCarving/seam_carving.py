72796a48a4b07522f4cd442a8d5b0c6f45e03605
Liron Cohen <lironcohen3@mail.tau.ac.il>
2022-03-23 13:19:30 +0200

    Update seam_carving.py

--- SeamCarving/SeamCarving/seam_carving.py
+++ SeamCarving/SeamCarving/seam_carving.py
@@ -1,7 +1,8 @@
 from typing import Dict, Any
+import utils
+import numpy as np
 
 NDArray = Any
-
 
 def resize(image: NDArray, out_height: int, out_width: int, forward_implementation: bool) -> Dict[str, NDArray]:
     """
@@ -16,5 +17,61 @@ def resize(image: NDArray, out_height: int, out_width: int, forward_implementati
             where img1 is the resized image and img2/img3 are the visualization images
             (where the chosen seams are colored red and black for vertical and horizontal seams, respecitvely).
     """
-    raise NotImplementedError('You need to implement this!')
+    gradients = utils.get_gradients(image)
+    original_height = image.shape[0]
+    original_width = image.shape[1]
+    height_diff = original_height - out_height
+    width_diff = original_width - out_width
+
+    grayscale_original_image = utils.to_grayscale(image)
+    if width_diff > 0: # need to scale down the width
+        (resized_width_image, vertical_seams) = scale_down(image, grayscale_original_image, gradients, width_diff, forward_implementation)
+    else: # need to scale up the width
+        (resized_width_image, vertical_seams) = scale_up(image, grayscale_original_image, abs(width_diff), forward_implementation)
+
+    grayscale_resized_image = utils.to_grayscale(resized_width_image)
+    np.rot90(image, k=1, axes=(0,1)) # rotating CCW
+    if height_diff > 0: # need to scale down the height
+        (resized_image, horizontal_seams) = scale_down(resized_width_image, grayscale_resized_image, height_diff, forward_implementation)
+    else: # need to scale up the height
+        (resized_image, horizontal_seams) = scale_up(resized_width_image, grayscale_resized_image, abs(height_diff), forward_implementation)
+    np.rot90(image, k=-1, axes=(0,1)) # rotating CW
+
+    out_images_dict = {'resized' : resized_image, 'vertical_seams' : vertical_seams ,'horizontal_seams' : horizontal_seams}
+
+    return out_images_dict
     # TODO: return { 'resized' : img1, 'vertical_seams' : img2 ,'horizontal_seams' : img3}
+
+def scale_down(original_image: NDArray, grayscale_image: NDArray, gradients: NDArray, dim_diff: int, is_forward : bool):
+    """
+    Scales down the width by dim_diff.
+    """
+    calculate_seams(grayscale_image, gradients, dim_diff, original_image.shape, is_forward)
+    resized_image = remove_seams_from_original()
+
+def calculate_seams(grayscale_image: NDArray, gradients: NDArray, dim_diff : int, original_shape: (int,int,int), is_forward: bool):
+    indices_matrix = np.indices((original_shape[0], original_shape[1]))[1]
+    for seam_number in range(dim_diff):
+        if is_forward:
+            cost_matrix = calculate_cost_matrix_forward(grayscale_image, gradients)
+            seam = find_best_seam_forward(cost_matrix)
+        else:
+            cost_matrix = calculate_cost_matrix_basic(grayscale_image)
+            seam = find_best_seam_basic(cost_matrix, gradients)
+        remove_seam_from_grayscale(grayscale_image, seam)
+
+def calculate_cost_matrix_basic(grayscale_image: NDArray, E: NDArray):
+    M = np.zeros_like(grayscale_image)
+    M[0,:] = E[0,:]
+    # column_256 = np.broadcast_to([256.], [grayscale_image.shape[1], 1])
+    for row in M[1:,:]:
+        shift_right_row = np.concatenate([256., M[row-1, 0:-1]], axis=1)
+        shift_left_row = np.concatenate([M[row-1, 1:], 256.], axis=1)
+        M[row] = E[row] + np.fmin(M[row-1,:], shift_left_row, shift_right_row)
+    return M
+
+
+
+
+
+
